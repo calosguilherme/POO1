05ad895bd84eba7d9b7e7022ef57879f133b1753
lukasg18 <lukas.gomes@hotmail.com>
2017-06-01 23:38:26 -0300

    classes exception criadas

---
+++ Faculdade/src/faculdade/ExDisciplinaInexistente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faculdade;
+
+/**
+ *
+ * @author lucas
+ */
+public class ExDisciplinaInexistente extends Exception{
+
+    public ExDisciplinaInexistente(String message) {
+        super(message);
+    }
+    
+}
